93ba8413507f242b83f3db21a61790691ad87b1e
Ismael R <ismael_br7@hotmail.com>
2023-11-11 17:43:45 -0600

    feature/banks => fix CORS type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,55 @@
+import { GetServerSideProps } from "next";
 import { Banks } from "@/containers/banks";
 import { useGlobalContext } from "@/context/global/useGlobalContext";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { MainLayout } from "@/layouts/MainLayout";
+import { setCookie } from "@/hooks/useCookies";
 
-function BanksPage() {
+import { getData } from "@/api/index";
+
+function BanksPage({ banks2 }: any) {
   const { banks } = useGlobalContext();
   const [storedBanks, setStoredBanks] = useLocalStorage("banks", []);
 
+  console.log("banks2", banks2);
+
   if (banks.length > 0 && storedBanks.length === 0) {
     setStoredBanks(banks);
+    setCookie("banks", JSON.stringify(true));
     localStorage.setItem("banks_time", new Date().toISOString());
   }
 
   return (
-    <MainLayout title="Banks" pageDescription="The best banks in Mexico">
-      <Banks banks={banks} />
-    </MainLayout>
+    <p>TEST</p>
+    // <MainLayout title="Banks" pageDescription="The best banks in Mexico">
+    //   <Banks banks={banks} />
+    // </MainLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  // const cookies = context.req.headers.cookie;
+  // console.log("cookies server =====>", cookies);
+
+  // if (cookies) {
+  //   return {
+  //     props: {
+  //       banks2: []
+  //     }
+  //   };
+  // }
+
+  const banksResponse = await getData(
+    "https://dev.obtenmas.com/catom/api/challenge/banks"
+  ).catch(err => {
+    console.log("ERROR", err);
+  });
+
+  return {
+    props: {
+      banks2: banksResponse
+    }
+  };
+};
 
 export default BanksPage;
